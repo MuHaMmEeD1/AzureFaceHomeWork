72d9c6857e39a75d9515b25e8d40c2ed9d213b7d
Muhamməd Nəbiyev <aliyevasamire596@gmail.com>
2024-12-05 17:22:04 +0400

    Add project files.

---
+++ AzureSpeechServiceHomeWork/Controllers/SpeechController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech;
+using System.Threading.Tasks;
+
+namespace AzureSpeechServiceHomeWork.Controllers
+{
+    
+
+    namespace YourProject.Controllers
+    {
+        public class SpeechController : Controller
+        {
+            private readonly IConfiguration _configuration;
+
+            public SpeechController(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> TextToSpeech(string text)
+            {
+                var speechKey = _configuration["AzureSpeech:Key"];
+                var speechRegion = _configuration["AzureSpeech:Region"];
+
+                var config = SpeechConfig.FromSubscription(speechKey, speechRegion);
+
+                using (var synthesizer = new SpeechSynthesizer(config))
+                {
+                    var result = await synthesizer.SpeakTextAsync(text);
+                    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                    {
+                        ViewBag.Message = "Speech synthesis succeeded!";
+                    }
+                    else
+                    {
+                        ViewBag.Message = $"Speech synthesis failed: {result.Reason}";
+                    }
+                }
+
+                return View("Index");
+            }
+        }
+    }
+
+}
